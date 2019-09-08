d1ed16743c856b35e35c2972905fdd03d7055720
John Adcock <adcockj@users.sourceforge.net>
2019-09-08 18:30:03 +0100

    Replace id with hash

--- jenkins_seed_job.groovy
+++ jenkins_seed_job.groovy
@@ -1,34 +1,48 @@
+// import required for md5 hash
+import java.security.MessageDigest
+
+
 // define a Job setting class
 class JobSetting {
     String UniqueID;
     String GitUrl;
     String JobName;
 	
-    JobSetting(String UniqueID, String GitUrl, String JobName) {
-        this.UniqueID = UniqueID
-        this.GitUrl = GitUrl
+    JobSetting(String GitProject, String JobName) {
+        this.UniqueID = CalcMD5(JobName)
+        this.GitUrl = "https://github.com/" + GitProject
         this.JobName = JobName
+    }
+
+    def CalcMD5(String s) {
+        MessageDigest.getInstance("MD5").digest(s.bytes).encodeHex().toString()
     }
 }
 
 // Create a list of job settings
 def job_settings = [
 // Add any new jobs following the pattern
-    new JobSetting('123456789', "https://github.com/JohnAdders/jenkinsfile_test.git", 'Jenkinsfile.test.project'),
-    new JobSetting('310370001', "https://github.com/JohnAdders/jenkinsfile_test.git", 'Jenkinsfile.test.project2'),
+    new JobSetting("JohnAdders/jenkinsfile_test.git", 'Jenkinsfile.test.project'),
+    new JobSetting("JohnAdders/jenkinsfile_test.git", 'Jenkinsfile.test.project2'),
 ]
 
 
 // create a Jenkins job multipipe job for each one
 // this uses the default setting which uses Jenkinsfile to control the pipeline
+// and keeps old branch builds around for a month
 for (job_setting in job_settings) {
     multibranchPipelineJob(job_setting.JobName) {
         branchSources {
             git {
-                id(job_setting.UniqueID) // IMPORTANT: use a constant and unique identifier
+                id(job_setting.UniqueID)
                 remote(job_setting.GitUrl)
             }
         }
+        orphanedItemStrategy {
+            discardOldItems {
+                daysToKeep(31)
+            }
+        }    
     }
 }
 
